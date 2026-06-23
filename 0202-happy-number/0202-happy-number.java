ecb486500227216f9e4c1078440ae10b5b1e6818
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-06-23 14:17:24 +0530

    Time: 0 ms (100%), Space: 42 MB (97.23%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,15 @@
 class Solution {
     public boolean isHappy(int n) {
-       int slow = sqr(n);
-       int fast = sqr(sqr(n));
+        int ans = 0;
 
-       while(slow != fast){
-            slow = sqr(slow);
-            fast = sqr(sqr(fast));
-         }
-           if(slow == 1){
-             return true;
-           }
-            return false;
-       }
-           public static int sqr(int number){
-           int ans = 0;
-           while(number > 0){
-           int rem = number % 10;
-           ans = ans + rem * rem;
-           number /= 10; 
-        }
-        return ans;
+         if(n == 1) return true;
+         if(n == 4) return false;
+         
+        while(n > 0){
+            int rem = n % 10 ;
+            ans += rem * rem;
+            n /= 10; 
+          }
+          return isHappy(ans);
         }
     }
