441b120ddd9ec9fba8762e432f9d8db0831e36b5
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-06-27 16:06:01 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int s = 0;
-        int e = nums.length-1;
+        // int s = 0;
+        // int e = nums.length-1;
 
-        while(s <= e){
-            int mid = s + (e - s)/2;
-               if(nums[mid] == target){
-                return mid;
-               }
-            if(nums[s] <= nums[mid]){
-                if(target >= nums[s] && target < nums[mid]){
-                e = mid -1;
-               }
-               else{
-                s = mid + 1; 
-               }
-            }else{
-                if(target <= nums[e] && target > nums[mid]){
-                    s = mid + 1; 
-                }else{
-                    e = mid -1;
-                }
-            }       
+        // while(s <= e){
+        //     int mid = s + (e - s)/2;
+        //        if(nums[mid] == target){
+        //         return mid;
+        //        }
+        //     if(nums[s] <= nums[mid]){
+        //         if(target >= nums[s] && target < nums[mid]){
+        //         e = mid -1;
+        //        }
+        //        else{
+        //         s = mid + 1; 
+        //        }
+        //     }else{
+        //         if(target <= nums[e] && target > nums[mid]){
+        //             s = mid + 1; 
+        //         }else{
+        //             e = mid -1;
+        //         }
+        //     }       
+        // }
+        // return -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]  == target){
+                return i;
+            }
         }
         return -1;
     }
