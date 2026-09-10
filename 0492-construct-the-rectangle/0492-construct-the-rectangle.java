2d830ffcf8d1be7c225e8f25fe7fe86d0648ed80
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-09-10 18:26:22 +0530

    Time: 0 ms (100%), Space: 42.5 MB (28.76%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+         int width = (int) Math.sqrt(area);
+
+        while (area % width != 0) {
+            width--;
+        }
+
+        int length = area / width;
+
+        return new int[]{length, width};
+    }
+}
